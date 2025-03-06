79599e4a67aaae13bb02846157c2f46e0f7fb9c1
Tony Fettes <tonyfettes@tonyfettes.com>
2025-03-06 23:23:10 +0800

    feat(build): search for CMakeLists.txt in deps

--- cmake/moonbit.cmake
+++ cmake/moonbit.cmake
@@ -19,6 +19,53 @@ function(setup_moonbit_module directory)
   endif()
   set(MOON_CURRENT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${MOON_CURRENT_SOURCE_DIR} PARENT_SCOPE)
   set(MOON_CURRENT_TARGET_DIR ${CMAKE_CURRENT_BINARY_DIR}/${MOON_CURRENT_TARGET_DIR} PARENT_SCOPE)
+  execute_process(COMMAND moon install)
+  string(JSON
+    MOON_MODULE_DEPS
+    ERROR_VARIABLE MOON_MODULE_DEPS_ERROR
+    GET ${MOON_MOD_JSON} "deps")
+  cmake_print_variables(MOON_MODULE_DEPS)
+  if(MOON_MODULE_DEPS_ERROR STREQUAL NOTFOUND)
+    string(JSON
+      MOON_MODULE_DEPS_LENGTH
+      ERROR_VARIABLE MOON_MODULE_DEPS_LENGTH_ERROR
+      LENGTH "${MOON_MODULE_DEPS}")
+    foreach(DEP RANGE 0 ${MOON_MODULE_DEPS_LENGTH})
+      string(JSON
+        MOON_MODULE_DEP
+        ERROR_VARIABLE MOON_MODULE_DEP_ERROR
+        MEMBER ${MOON_MODULE_DEPS} ${DEP})
+      cmake_print_variables(MOON_MODULE_DEP)
+      if(MOON_MODULE_DEP_ERROR STREQUAL NOTFOUND)
+        string(JSON
+          MOON_MODULE_DEP_TYPE
+          ERROR_VARIABLE MOON_MODULE_DEP_TYPE_ERROR
+          TYPE "${MOON_MODULE_DEPS}" "${MOON_MODULE_DEP}")
+        cmake_print_variables(MOON_MODULE_DEP_TYPE)
+        if(MOON_MODULE_DEP_TYPE STREQUAL "STRING")
+          set(MOON_MODULE_DEP_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${directory}/.mooncakes/${MOON_MODULE_DEP})
+          cmake_print_variables(MOON_MODULE_DEP_PATH)
+          if(EXISTS ${MOON_MODULE_DEP_PATH}/CMakeLists.txt)
+            message("found ${MOON_MODULE_DEP_PATH}/CMakeLists.txt")
+            add_subdirectory(${MOON_MODULE_DEP_PATH})
+          else()
+            message("not found ${MOON_MODULE_DEP_PATH}/CMakeLists.txt")
+          endif()
+        else()
+          string(JSON
+            MOON_MODULE_DEP_PATH
+            ERROR_VARIABLE MOON_MODULE_DEP_PATH_ERROR
+            GET "${MOON_MODULE_DEP}" "path")
+          cmake_print_variables(MOON_MODULE_DEP_PATH)
+          if(MOON_MODULE_DEP_PATH_ERROR STREQUAL NOTFOUND)
+            if(EXISTS ${MOON_MODULE_DEP_PATH}/CMakeLists.txt)
+              add_subdirectory(${MOON_MODULE_DEP_PATH})
+            endif()
+          endif()
+        endif()
+      endif()
+    endforeach()
+  endif()
 endfunction()
 
 function(add_moon_executable target_name)
