bf90fdb24ca201f7b7a5ecc83817db5409123e86
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-03-23 21:39:20 +0800

    fix: clean up resources

--- test/tcp-client/main.c
+++ test/tcp-client/main.c
@@ -1,5 +1,6 @@
 #include "uv.h"
 #include "uv/unix.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,37 +12,35 @@ typedef struct tty_write_req_s {
   uv_write_t write;
 } tty_write_req_t;
 
-typedef struct tty_read_req_s {
-  uv_fs_t req;
-  uv_stream_t *stream;
-  char data[1024];
-} tty_read_req_t;
+typedef struct stdin_tty_s {
+  uv_tty_t tty;
+  uv_stream_t *server;
+} stdin_tty_t;
 
-typedef struct tcp_write_req_s {
+typedef struct tcp_write_s {
   uv_write_t write;
-  tty_read_req_t *tty_read_req;
-} tcp_write_req_t;
+  stdin_tty_t *tty_read_req;
+} tcp_write_t;
+
+typedef struct tcp_server_s {
+  uv_stream_t *server;
+  uv_tty_t *stdin_tty;
+} tcp_server_t;
 
 void
-on_tty_read(uv_fs_t *req);
+on_tty_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf);
 
 void
-on_close(uv_handle_t *handle) {
+on_tcp_close(uv_handle_t *handle) {
   free(handle);
 }
 
 void
 on_tcp_write(uv_write_t *req, int status) {
-  tcp_write_req_t *write = containerof(req, tcp_write_req_t, write);
+  tcp_write_t *write = containerof(req, tcp_write_t, write);
   if (status < 0) {
     fprintf(stderr, "Write error: %s\n", uv_strerror(status));
   }
-  uv_buf_t tty_read_buf =
-    uv_buf_init(write->tty_read_req->data, sizeof(write->tty_read_req->data));
-  uv_fs_read(
-    uv_default_loop(), &write->tty_read_req->req, 0, &tty_read_buf, 1, -1,
-    on_tty_read
-  );
   free(write);
 }
 
@@ -53,14 +52,19 @@ alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
 }
 
 void
-on_tty_read(uv_fs_t *req) {
-  tty_read_req_t *tty_read_req = containerof(req, tty_read_req_t, req);
-  ssize_t nread = req->result;
+on_tty_close(uv_handle_t *handle) {
+  stdin_tty_t *stdin_tty = containerof(handle, stdin_tty_t, tty);
+  free(stdin_tty);
+}
+
+void
+on_tty_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+  stdin_tty_t *stdin_tty = containerof(stream, stdin_tty_t, tty);
   if (nread > 0) {
-    tcp_write_req_t *write = malloc(sizeof(tcp_write_req_t));
-    write->tty_read_req = tty_read_req;
-    uv_buf_t buf = uv_buf_init(tty_read_req->data, nread);
-    uv_write(&write->write, tty_read_req->stream, &buf, 1, on_tcp_write);
+    tcp_write_t *write = malloc(sizeof(tcp_write_t));
+    write->tty_read_req = stdin_tty;
+    uv_buf_t write_buf = uv_buf_init(buf->base, nread);
+    uv_write(&write->write, stdin_tty->server, &write_buf, 1, on_tcp_write);
     return;
   }
   if (nread < 0) {
@@ -69,9 +73,12 @@ on_tty_read(uv_fs_t *req) {
     } else {
       fprintf(stderr, "Read EOF\n");
     }
-    uv_close((uv_handle_t *)tty_read_req->stream, on_close);
   }
-  free(tty_read_req);
+  uv_read_stop(stdin_tty->server);
+  uv_close((uv_handle_t *)stdin_tty->server, on_tcp_close);
+  uv_read_stop(stream);
+  uv_close((uv_handle_t *)stream, on_tty_close);
+  uv_stop(uv_default_loop());
 }
 
 void
@@ -84,12 +91,14 @@ on_tty_write(uv_fs_t *req) {
 
 void
 on_tcp_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
+  tcp_server_t *server = containerof(stream, tcp_server_t, server);
   if (nread > 0) {
     uv_fs_t *req = malloc(sizeof(uv_fs_t));
     uv_buf_t write_buf = uv_buf_init(buf->base, nread);
     uv_fs_write(
       uv_default_loop(), req, STDOUT_FILENO, &write_buf, 1, -1, on_tty_write
     );
+    return;
   }
   if (nread < 0) {
     if (nread != UV_EOF) {
@@ -97,8 +106,12 @@ on_tcp_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
     } else {
       fprintf(stderr, "Read EOF\n");
     }
-    uv_close((uv_handle_t *)stream, NULL);
   }
+  uv_read_stop(stream);
+  uv_close((uv_handle_t *)stream, on_tcp_close);
+  uv_read_stop((uv_stream_t *)server->stdin_tty);
+  uv_close((uv_handle_t *)server->stdin_tty, on_tty_close);
+  uv_stop(uv_default_loop());
 }
 
 void
@@ -107,16 +120,17 @@ on_connect(uv_connect_t *connect, int status) {
     fprintf(stderr, "Connect error: %s\n", uv_strerror(status));
     return;
   }
-  uv_stream_t *stream = connect->handle;
-  tty_read_req_t *tty_read_req = malloc(sizeof(tty_read_req_t));
-  tty_read_req->stream = stream;
-  uv_read_start(stream, alloc_buffer, on_tcp_read);
-  uv_buf_t tty_read_buf =
-    uv_buf_init(tty_read_req->data, sizeof(tty_read_req->data));
-  uv_fs_read(
-    uv_default_loop(), &tty_read_req->req, STDIN_FILENO, &tty_read_buf, 1, -1,
-    on_tty_read
-  );
+
+  tcp_server_t *server = malloc(sizeof(tcp_server_t));
+  server->server = connect->handle;
+
+  stdin_tty_t *stdin_tty = malloc(sizeof(stdin_tty_t));
+  uv_tty_init(uv_default_loop(), &stdin_tty->tty, STDOUT_FILENO, true);
+
+  server->stdin_tty = &stdin_tty->tty;
+  stdin_tty->server = connect->handle;
+  uv_read_start(server->server, alloc_buffer, on_tcp_read);
+  uv_read_start((uv_stream_t *)&stdin_tty->tty, alloc_buffer, on_tty_read);
 }
 
 int
