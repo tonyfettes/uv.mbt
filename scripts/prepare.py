a4419e75796cef0c893377fff5057d84acf52524
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-03-26 22:06:10 +0800

    fix: relocate headers

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -1,4 +1,6 @@
 import platform
+import json
+import re
 
 uv_defines = []
 uv_sources = [
@@ -84,16 +86,16 @@ else:
         "src/unix/udp.c",
     ]
 
-if platform.platform() in ["Darwin", "Linux"]:
+if platform.system() in ["Darwin", "Linux"]:
     uv_sources += ["src/unix/proctitle.c"]
 
-if platform.platform() == "Darwin":
+if platform.system() == "Darwin":
     uv_sources += ["src/unix/kqueue.c"]
 
-if platform.platform() == "Darwin":
+if platform.system() == "Darwin":
     uv_sources += ["src/unix/random-getentropy.c"]
 
-if platform.platform() == "Darwin":
+if platform.system() == "Darwin":
     uv_defines += ["_DARWIN_UNLIMITED_SELECT=1", "_DARWIN_USE_64_BIT_INODE=1"]
     uv_sources += [
         "src/unix/darwin-proctitle.c",
@@ -101,7 +103,7 @@ if platform.platform() == "Darwin":
         "src/unix/fsevents.c",
     ]
 
-if platform.platform() == "Linux":
+if platform.system() == "Linux":
     uv_defines += ["_GNU_SOURCE", "_POSIX_C_SOURCE=200112"]
     uv_libraries += ["dl", "rt"]
     uv_sources += [
@@ -120,44 +122,94 @@ if uv_lib_directory.exists():
     shutil.rmtree(uv_lib_directory)
 
 uv_lib_directory.mkdir(parents=True, exist_ok=True)
-
 (uv_lib_directory / ".gitignore").write_text("\n".join(["*.c", "*.h"]) + "\n")
+(uv_lib_directory / "moon.pkg.json").write_text("{}\n")
 
-shutil.copy(uv_directory / "include" / "uv.h", uv_lib_directory / "uv.h")
-shutil.copytree(uv_directory / "include" / "uv", uv_lib_directory / "uv")
+
+def create_directory(directory: Path):
+    print(f"create directory {directory}")
+    directory.mkdir(parents=True, exist_ok=True)
+
+
+def moon_pkg_json_add_native_stub(moon_pkg_path: Path, native_stub: str):
+    moon_pkg_json = json.loads(moon_pkg_path.read_text())
+    if "native-stub" not in moon_pkg_json:
+        moon_pkg_json["native-stub"] = []
+    moon_pkg_json["native-stub"].append(native_stub)
+    moon_pkg_path.write_text(json.dumps(moon_pkg_json, indent=2) + "\n")
+
+
+def copy_header_with_relocated_headers(source: Path, destination: Path):
+    print(f"copy {source} -> {destination}")
+    content = source.read_text()
+
+    def relocate(match: re.Match) -> str:
+        header = match.group(1)
+        if (uv_directory / "src" / header).exists():
+            header = (uv_directory / "src").relative_to(source.parent, walk_up=True) / header
+            return f'#include "{header.as_posix()}"'
+        if (uv_directory / "include" / header).exists():
+            header = (uv_directory / "src").relative_to(source.parent, walk_up=True) / header
+            return f'#include "{header.as_posix()}"'
+        return f'#include "{header}"'
+
+    content = re.sub(r'#include "(.*?)"', relocate, content)
+    if not destination.parent.exists():
+        create_directory(destination.parent)
+    destination.write_text(content)
+
 
 # Copy headers
 for header in (uv_directory / "src").rglob("*.h"):
     relative_path = header.relative_to(uv_directory / "src")
     destination = uv_lib_directory / relative_path
-    if not destination.parent.exists():
-        destination.parent.mkdir(parents=True, exist_ok=True)
-    if destination.exists():
-        raise FileExistsError(f"File {destination} already exists")
-    print(f"Copying {header} to {destination}")
-    shutil.copy(header, destination)
+    copy_header_with_relocated_headers(header, destination)
 
-native_stubs = []
+
+def copy_source_with_relocated_headers(source: Path, destination: Path):
+    print(f"copy {source} -> {destination}")
+    content = source.read_text()
+
+    def relocate(match: re.Match) -> str:
+        header = match.group(1)
+        if (uv_directory / "src" / header).exists():
+            return f'#include "{header}"'
+        if (uv_directory / "include" / header).exists():
+            return f'#include "{header}"'
+        hypothesis = (source.parent.relative_to(uv_directory / "src")) / header
+        header = hypothesis.as_posix()
+        return f'#include "{header}"'
+
+    content = re.sub(r'#include "(.*?)"', relocate, content)
+    if not destination.parent.exists():
+        create_directory(destination.parent)
+    destination.write_text(content)
+
+
 # Copy files
+native_stubs = []
 for source in uv_sources:
     source_path = uv_directory / source
     destination = uv_lib_directory / source_path.name
-    native_stubs.append(source_path.name)
-    if destination.exists():
-        raise FileExistsError(f"File {destination} already exists")
-    print(f"Copying {source_path} to {destination}")
-    shutil.copy(source_path, destination)
-native_stubs.append("uv-lib.c")
+    copy_source_with_relocated_headers(source_path, destination)
+    native_stubs.append(destination.name)
 
-import json
 
-(uv_lib_directory / "moon.pkg.json").write_text(
-    json.dumps({"native-stub": native_stubs}, indent=2) + "\n"
-)
+# Copy API header
+shutil.copy(uv_directory / "include" / "uv.h", uv_lib_directory / "uv.h")
+shutil.copytree(uv_directory / "include" / "uv", uv_lib_directory / "uv")
+(uv_lib_directory / "uv" / "uv").symlink_to(".")
+
 
 src_directory = Path("src")
-
 shutil.copy(src_directory / "uv.c", uv_lib_directory / "uv-lib.c")
+native_stubs.append("uv-lib.c")
+moon_pkg_json = {
+    "native-stub": native_stubs,
+}
+(uv_lib_directory / "moon.pkg.json").write_text(
+    json.dumps(moon_pkg_json, indent=2) + "\n"
+)
 
 moon_pkg_path = src_directory / "moon.pkg.json"
 moon_pkg_json = json.loads(moon_pkg_path.read_text())
