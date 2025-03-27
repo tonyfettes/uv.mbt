c6a7acaf63b1621e00f459c774984e6e99e0261b
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-03-27 13:58:31 +0800

    fix(ci): link system libraries on Windows

--- scripts/cmake.py
+++ scripts/cmake.py
@@ -14,7 +14,19 @@ def generate_native_flags_to(moon_pkg_path: Path):
             moon_pkg_json["link"]["native"]["cc-link-flags"] = "src/uv-install/lib/libuv.a"
         elif platform.system() == "Windows":
             moon_pkg_json["link"]["native"]["stub-cc-flags"] = "/Isrc\\uv-install\\include"
-            moon_pkg_json["link"]["native"]["cc-link-flags"] = "src\\uv-install\\lib\\libuv.lib"
+            external_libraries = [
+                "psapi",
+                "user32",
+                "advapi32",
+                "iphlpapi",
+                "userenv",
+                "ws2_32",
+                "dbghelp",
+                "ole32",
+                "shell32",
+            ]
+            cc_link_flags = " ".join([f"/link {lib}.lib" for lib in external_libraries])
+            moon_pkg_json["link"]["native"]["cc-link-flags"] = f"src\\uv-install\\lib\\libuv.lib {cc_link_flags}"
         else:
             raise NotImplementedError(f"Unsupported platform: {platform.system()}")
     moon_pkg_path.write_text(
