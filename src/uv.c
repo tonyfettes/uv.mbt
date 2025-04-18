59a1a5f612502e249ed5a26ae1861b2533d7aea8
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-04-18 16:45:24 +0800

    feat: uv_shutdown

--- src/uv.c
+++ src/uv.c
@@ -1898,3 +1898,59 @@ moonbit_uv_os_uname(moonbit_bytes_t buffer) {
   moonbit_decref(buffer);
   return status;
 }
+
+typedef struct moonbit_uv_shutdown_s {
+  uv_shutdown_t shutdown;
+} moonbit_uv_shutdown_t;
+
+static inline void
+moonbit_uv_shutdown_finalize(void *object) {
+  moonbit_uv_shutdown_t *shutdown = object;
+  if (shutdown->shutdown.data) {
+    moonbit_decref(shutdown->shutdown.data);
+  }
+}
+
+MOONBIT_FFI_EXPORT
+moonbit_uv_shutdown_t *
+moonbit_uv_shutdown_make(void) {
+  moonbit_uv_shutdown_t *shutdown =
+    (moonbit_uv_shutdown_t *)moonbit_make_external_object(
+      moonbit_uv_shutdown_finalize, sizeof(moonbit_uv_shutdown_t)
+    );
+  memset(shutdown, 0, sizeof(moonbit_uv_shutdown_t));
+  return shutdown;
+}
+
+typedef struct moonbit_uv_shutdown_cb_s {
+  int32_t (*code)(
+    struct moonbit_uv_shutdown_cb_s *,
+    moonbit_uv_shutdown_t *req,
+    int status
+  );
+} moonbit_uv_shutdown_cb_t;
+
+static inline void
+moonbit_uv_shutdown_cb(uv_shutdown_t *req, int status) {
+  moonbit_uv_shutdown_cb_t *cb = req->data;
+  req->data = NULL;
+  moonbit_uv_shutdown_t *shutdown =
+    containerof(req, moonbit_uv_shutdown_t, shutdown);
+  cb->code(cb, shutdown, status);
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_uv_shutdown(
+  moonbit_uv_shutdown_t *req,
+  uv_stream_t *handle,
+  moonbit_uv_shutdown_cb_t *cb
+) {
+  if (req->shutdown.data) {
+    moonbit_decref(req->shutdown.data);
+  }
+  req->shutdown.data = cb;
+  int32_t status = uv_shutdown(&req->shutdown, handle, moonbit_uv_shutdown_cb);
+  moonbit_decref(handle);
+  return status;
+}
